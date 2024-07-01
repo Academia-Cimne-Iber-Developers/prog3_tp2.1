05dea7bafc4a9a27c29a91c2b7bb2312268b68cf
Luciano Flores <98068602+lucianorafaelflores@users.noreply.github.com>
2024-07-01 17:36:35 -0300

    creamos el setter updateValue

--- ejercicio1/sensors.js
+++ ejercicio1/sensors.js
@@ -1,12 +1,16 @@
 class Sensor {
-    constructor(id, name, type, value, unit, updated_at){
+    constructor(id, name, type, value, unit, updated_at = null){
         this.id=id;
         this.name=name;
         this.type=type;
         this.value=value;
         this.unit=unit;
-        this.updated_at=updated_at;
-
+        this.updated_at=updated_at || new Date().toISOString();
+    }
+    set updateValue(newValue){
+        this.value= newValue;
+        this.updated_at = new Date().toISOString();
+        
     }
 }
 
